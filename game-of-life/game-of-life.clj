75417bc84682ad3b7cc25599330404195e3c24ee
DjebbZ <khalid.jebbari@gmail.com>
2013-12-05 20:10:33 +0100

    Create a lein project

--- game-of-life/game-of-life.clj
+++ game-of-life/game-of-life.clj
@@ -1,6 +1,5 @@
-
-
-(ns NightClazz1.core)
+(ns NightClazz1.core
+  (:require [quil.core :as q]))
 
 ;; cellule
 [0 1]
@@ -12,3 +11,11 @@
 
  ;; génération -> ensemble des cellules vivantes
 (def world #{[0 1] [1 2] [1 1]})
+
+;; setup
+(defn setup []
+  (q/smooth)
+  (q/frame-rate 30))
+
+(defn draw []
+  (q/background (q/color 0 0 255)))
